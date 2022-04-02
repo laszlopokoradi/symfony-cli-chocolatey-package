580b61131fc4e83651ee032ffdf55ef9aa862ac5
Laszlo Pokoradi <laszlo.pokoradi@gmail.com>
2022-04-02 17:22:32 +0200

    First Beta version

--- tools/chocolateybeforemodify.ps1
+++ tools/chocolateybeforemodify.ps1
@@ -1,9 +1,24 @@
-﻿# This runs in 0.9.10+ before upgrade and uninstall.
-# Use this file to do things like stop services prior to upgrade or uninstall.
-# NOTE: It is an anti-pattern to call chocolateyUninstall.ps1 from here. If you
-#  need to uninstall an MSI prior to upgrade, put the functionality in this
-#  file without calling the uninstall script. Make it idempotent in the
-#  uninstall script so that it doesn't fail when it is already uninstalled.
-# NOTE: For upgrades - like the uninstall script, this script always runs from 
-#  the currently installed version, not from the new upgraded package version.
+﻿$softwareName = 'symfony-cli'
 
+$programFiles = (${env:ProgramFiles},$null -ne ${env:ProgramFiles(x86)})[0]
+$installDir = "$programFiles\$softwareName"
+
+$statementTerminator = ";"
+
+$actualPath = [System.Collections.ArrayList](Get-EnvironmentVariable -Name 'Path' -Scope 'Machine' -PreserveVariables).split($statementTerminator)
+
+if ($actualPath -contains $installDir)
+{
+	Write-Host "PATH environment variable contains $installDir. Removing..."
+	
+	$actualPath.Remove($installDir)	
+	$newPath =  $actualPath -Join $statementTerminator
+
+	$cmd = "Set-EnvironmentVariable -Name 'Path' -Value `'$newPath`' -Scope 'Machine'"
+
+    if (Test-ProcessAdminRights) {
+        Invoke-Expression $cmd
+    } else {
+        Start-ChocolateyProcessAsAdmin "$cmd"
+    }
+}
