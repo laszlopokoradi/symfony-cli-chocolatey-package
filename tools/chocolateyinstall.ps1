16d0e9f71f5510dec57712eb5fa1c7c070fc361f
Laszlo Pokoradi <laszlo.pokoradi@sixsentix.com>
2022-04-11 10:23:53 +0200

    Symfony CLI v5.4.7 update

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -6,9 +6,9 @@ $installDir = "$programFiles\$softwareName"
 $baseURL    = "https://github.com/symfony-cli/symfony-cli/releases/download/"
 
 $checksumType = 'sha256'
-$checksum   = "453b00c27c770ce7f16ad8215e4026ac8b4065f870b2c1b5cd66e2270895f496"
+$checksum   = "1e963e86d57c890d8beaaae3d89cf1eb91bb8b26c9447c988d366561df33fe0a"
 $fileName   = "symfony-cli_windows_386.zip"
-$checksum64 = "6215248289dd53ca40ad5a79898a2bde4849a3842171d8a325f97956575eb48f"
+$checksum64 = "a77527beeac8f11b56693d88523e8b32ff9c657a40432159444d0d0f6a0de220"
 $fileName64 = "symfony-cli_windows_amd64.zip"
 
 $url        = $baseURL + "v" + $version + "/" + $fileName
