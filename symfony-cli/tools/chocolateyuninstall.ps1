1db8cf65c19dca26fc3eb7262228f03ed514fefd
lpokoradi <laszlo.pokoradi@gmail.com>
2023-03-30 16:37:42 +0200

    Restructure for automatic updates

---
+++ symfony-cli/tools/chocolateyuninstall.ps1
@@ -0,0 +1,4 @@
+﻿$programFiles = (${env:ProgramFiles},$null -ne ${env:ProgramFiles(x86)})[0]
+$installDir = "$programFiles\$env:ChocolateyPackageName"
+
+Remove-Item -Path $installDir -Recurse
--- tools/chocolateyuninstall.ps1
+++
@@ -1,4 +0,0 @@
-﻿$programFiles = (${env:ProgramFiles},$null -ne ${env:ProgramFiles(x86)})[0]
-$installDir = "$programFiles\$env:ChocolateyPackageName"
-
-Remove-Item -Path $installDir -Recurse
